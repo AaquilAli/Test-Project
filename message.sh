826d247478d94963e2052f9a6ae1999fa1113eb5
Aaquil <aaquilali2050@gmail.com>
2017-09-27 12:11:13 +0530

    first shell

---
+++ message.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#notify-send --expire-time=1000  'Info' 'Device Not Found'
+
+echo 'notify-send "Meeting in 1 hour with the big boss!"' | at 1pm tomorrow 
+
+#notify-send 'Info' 'Bye'
