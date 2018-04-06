cd27ff000b87719cd8db93bc7e28b9becc5457d3
Aaquil Ali <aaquilali2050@gmail.com>
2018-04-06 16:34:41 +0530

    adfas

--- curlTest.cpp
+++ curlTest.cpp
@@ -4,6 +4,8 @@
 int main(void)
 {
 
+sting hello = "hello world";
+
 string json_struct = "{ "tenantName": "SIE","serviceName": "COR","applianceId": "","rdCategory": "D2P","rdSubCategory": "BIZ","rdType": "FNCPRHT","rdSubType": "NULL","deviceId":"00000000534de1ee","timeofTransaction": 1449659076058,"txnLocation": "PashanDesk","txnLat": "NA","txnLong": "NA","statusCode": "0","statusSubCode": "NULL","authToken": "","payLoad": [ {"txCat": "NULL","txSubCat": "NULL","paramName": "Temperature Value","paramValue": "23.6","paramType": "FLOAT","alertFlag": "0","statusCode": "0","statusSubCode": "NULL","txTimeStamp": 1449658837000},{"txCat": "NULL","txSubCat": "NULL","paramName": "Humidity Value","paramValue": "41.43","paramType":"FLOAT","alertFlag": "0","statusCode": "0","statusSubCode": "NULL","txTimeStamp": 1449658837000},{"txCat": "NULL","txSubCat": "NULL","paramName": "Temperature Sensor Status","paramValue": "0","paramType":"TEXT","alertFlag": "0","statusCode": "0","statusSubCode": "NULL","txTimeStamp": 1449658837000}],"otherDetails":null}"
 curl = curl_easy_init();
 
